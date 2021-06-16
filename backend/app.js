c33e61713acf3dfaa791d1641e82c736e5bb6f61
Shubham Dave <shubham.dave001@gmail.com>
2021-06-16 18:49:21 +0530

    Changes for index.html. Changes for app file for port and changes.

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: true});
 //app.use(bodyParser.json());
 
-app.use("/images", express.static(path.join("images")));
+// app.use("/images", express.static(path.join("images")));
 
 // Set Static access for angular
 app.use("/", express.static(path.join(__dirname, "angular")));
