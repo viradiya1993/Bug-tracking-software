c3b7168815d2a1deaf220c2e909c766aadfadeb3
Shubham Dave <shubham.dave001@gmail.com>
2021-06-18 17:58:06 +0530

    Changes for build upload.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ded3138.inmotionhosting.com/~projectdemoserve/bug_tracking_software/api'
+  apiUrl: 'https://ded3138.inmotionhosting.com:4567/~projectdemoserve/bug_tracking_software/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  // apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://ded3138.inmotionhosting.com:4567/~projectdemoserve/bug_tracking_software/api'
 };
