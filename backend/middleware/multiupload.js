59b804934cfd12fad1cb80b8f7770aa371fc5de7
Gaurav Prajapati <mt_063@outlook.com>
2021-07-16 20:01:44 +0530

    changes - upload multiple image

--- backend/middleware/multiupload.js
+++ backend/middleware/multiupload.js
@@ -1,17 +1,24 @@
 const multer = require("multer");
+const fs = require("fs")
 
-const MIME_TYPE_MAP = {
-	'image/png': 'png',
-	'image/jpeg': 'jpg',
-	'image/jpg': 'jpg'
-}
+// const MIME_TYPE_MAP = {
+// 	'image/png': 'png',
+// 	'image/jpeg': 'jpg',
+// 	'image/jpg': 'jpg'
+// }
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		const isValid = MIME_TYPE_MAP[file.mimetype];
-		let error = new Error("Invalid Image MIME type");
-		if (isValid) {
-				error = null;
+		console.log('file: ', file);
+		// const isValid = MIME_TYPE_MAP[file.mimetype];
+		// let error = new Error("Invalid Image MIME type");
+		// if (isValid) {
+		// 		error = null;
+		// }
+		let error = null;
+		var dir = 'backend/images';
+		if (!fs.existsSync(dir)){
+			fs.mkdirSync(dir);
 		}
 		cb(error, "backend/images") 
 	},
